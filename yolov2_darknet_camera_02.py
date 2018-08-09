7d186b50bfbe17b9cdad332300a30f6bdb4b15f0
ryu-ml <sky-programmer@outlook.jp>
2018-08-09 17:35:48 +0900

    First commit

---
+++ yolov2_darknet_camera_02.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from chainer import serializers, Variable
+import chainer.functions as F
+from yolov2_darknet_predict import CocoPredictor
+
+cap = cv2.VideoCapture(0)
+coco_predictor = CocoPredictor()
+
+in_key = None
+fin_flag = False
+
+while(not in_key == 27 and not fin_flag):
+    ret, orig_img = cap.read()
+    nms_results = coco_predictor(orig_img)
+
+    # draw result
+    for result in nms_results:
+        left, top = result["box"].int_left_top()
+        right, bottom = result["box"].int_right_bottom()
+        cv2.rectangle(orig_img, (left, top), (right, bottom), (255, 0, 255), 3)
+        text = '%s(%2d%%)' % (result["label"], result["probs"].max()*result["conf"]*100)
+        cv2.putText(orig_img, text, (left, top-7), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        print(text)
+
+    cv2.imshow("w", orig_img)
+    in_key = cv2.waitKey(1)
+    if in_key == 27:
+        # finish capture
+        cap.release()
+        cv2.destroyAllWindows()
+        fin_flag = True
+        break
+
